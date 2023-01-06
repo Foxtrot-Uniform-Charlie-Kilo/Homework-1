7528331d8afff648276beaa422265f65a70bca2e
maxim <maximus77t@gmail.com>
2023-01-06 23:03:55 +0300

    Два числа min и max

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 0)
+Console.Write("Введите число №1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
 {
-    Console.Write("Число четное");
+    Console.WriteLine("Максимально число = "+num1);
+    Console.WriteLine("Минимальное число = "+num2);
 }
 else
 {
-    Console.Write("Число нечетное");
+    Console.WriteLine("Максимально число = "+num2);
+    Console.WriteLine("Минимальное число = "+num1);
 }
