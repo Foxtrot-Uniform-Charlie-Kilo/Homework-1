5953bdf2474b22ef934fa5ce00067b0eae30de63
maxim <maximus77t@gmail.com>
2023-01-06 23:13:52 +0300

    Три числа и Max из них

--- Program.cs
+++ Program.cs
@@ -4,14 +4,19 @@ Console.Write("Введите число №1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число №2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
+if (num1 > num2 && num1 > num3)
 {
     Console.WriteLine("Максимально число = "+num1);
-    Console.WriteLine("Минимальное число = "+num2);
 }
-else
+
+if (num2 > num1 && num2 > num3)
 {
     Console.WriteLine("Максимально число = "+num2);
-    Console.WriteLine("Минимальное число = "+num1);
+}
+if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("Максимально число = "+num3);
 }
