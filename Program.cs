631d808587c684208d531188aa093ed122e0a2e0
maxim <maximus77t@gmail.com>
2023-01-06 22:33:15 +0300

    Третья цифра

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.Write("Число не трехзначное");
+}
