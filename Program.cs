f9798d3e329d40298a90fea7afbc62c37fc414c8
maxim <maximus77t@gmail.com>
2023-01-17 17:56:02 +0300

    Task4 redone

--- Program.cs
+++ Program.cs
@@ -1,22 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Введите число №1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число №2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число №3: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-
-if (num1 > num2 && num1 > num3)
-{
-    Console.WriteLine("Максимально число = "+num1);
-}
-
-if (num2 > num1 && num2 > num3)
-{
-    Console.WriteLine("Максимально число = "+num2);
-}
-if (num3 > num1 && num3 > num2)
-{
-    Console.WriteLine("Максимально число = "+num3);
-}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());;
+    
+for (int i = 1; i <= num; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
